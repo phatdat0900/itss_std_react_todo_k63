56fce135a158e1a5c1f460cc42d5a94c4e20154b
datletan <>
2022-06-06 04:17:22 +0000

    Save todo

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -19,13 +19,25 @@ import useStorage from '../hooks/storage';
 import {getKey} from "../lib/util";
 
 function Todo() {
-  const [items, putItems] = React.useState([
-      /* テストコード 開始 */
-    { key: getKey(), text: '日本語の宿題', done: false },
-    { key: getKey(), text: 'reactを勉強する', done: false },
-    { key: getKey(), text: '明日の準備をする', done: false },
-    /* テストコード 終了 */
-  ]);
+  // const [items, putItems] = React.useState([
+  //     /* テストコード 開始 */
+  //   { key: getKey(), text: '日本語の宿題', done: false },
+  //   { key: getKey(), text: 'reactを勉強する', done: false },
+  //   { key: getKey(), text: '明日の準備をする', done: false },
+  //   /* テストコード 終了 */
+  // ]);
+
+  const [content, setContent] = useState('');
+  const [tabSelected, setTabSelected] = useState(0);
+  const [items, putItems, clearItems] = useStorage();
+
+  const tabs = ['全て', '未完了', '完了済み'];
+
+  const onSubmit = () => {
+    putItems([...items, { key: getKey(), text: content, done: false }]);
+    setContent('');
+  }
+
   const onUpdate = (data) => {
     const index = items.findIndex(item => item.key === data.key);
     const new_arr = [...items];
@@ -34,19 +46,7 @@ function Todo() {
       putItems([...new_arr]);
     }
   };
-const [content, setContent] = useState('');
-  const [tabSelected, setTabSelected] = useState(0);
 
-  useEffect(() => {
-
-  }, []);
-
-  const tabs = ['全て', '未完了', '完了済み'];
-
-  const onSubmit = () => {
-    putItems([...items, { key: getKey(), text: content, done: false }]);
-    setContent('');
-  }
   const getLists = () => {
     switch(tabSelected){
       case 0: return items;
@@ -56,9 +56,10 @@ const [content, setContent] = useState('');
     }
   }
 
-  //  const onRemove = () => {
-  //   clearItems();
-  // } 
+  const onRemove = () => {
+    clearItems();
+  } 
+
   return (
     <div className="panel">
       <div className="panel-heading">
@@ -67,24 +68,34 @@ const [content, setContent] = useState('');
       <input type='text'
         value={content}
         onChange={e => setContent(e.target.value)}
+        placeholder='Todoを入力してください'
         onKeyUp={e => {
           if(e.key === 'Enter')
             onSubmit()
         }}/>
-            <div className='tabs'>
+      
+      <div className='tabs'>
         {
           tabs.map((tab, index) => (<div className={`tab ${index === tabSelected ? 'active' : ''}`} key={index} onClick={() => setTabSelected(index)} >{tab}</div>))
         }
       </div>
-
+      
       {getLists().map(item => (
         <label key={item.key} className="panel-block">
           <TodoItem item={item} onClick={(data) => onUpdate(data)} />
         </label>
       ))}
+
       <div className="panel-block">
         {items.length} items
       </div>
+
+      <input
+        type='submit'
+        value='全てTodoを削除'
+        onClick={onRemove}
+      />
+      
     </div>
   );
 }
--- src/hooks/storage.js
+++ src/hooks/storage.js
@@ -15,18 +15,20 @@ function useStorage() {
   const [items, setItems] = useState([]);
 　
 　/* 副作用を使う */
-  useEffect(() => {
-    
+ useEffect(() => {
+    const storage = JSON.parse(localStorage.getItem(STORAGE_KEY)) || [];
+    setItems(storage);
   }, []);
 
   const putItems = items => {
-    
+    setItems([...items]);
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(items));
   };
 
   const clearItems = () => {
-    
+    setItems([])
+    localStorage.removeItem(STORAGE_KEY);
   };
-
   return [items, putItems, clearItems];
 }
 
